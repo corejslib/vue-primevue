c7ae980dbf03c041007e4ca7752c13752f10a8a2
zdm <zdm@softvisio.net>
2026-08-11 11:07:16 +0300

    chore: init

--- src/app/viewport.js
+++ src/app/viewport.js
@@ -1,10 +1,6 @@
-import "#ext";
-import "#ewc";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import VueViewport from "@corejslib/vue/app/viewport";
-
-// must be impoerted after ExtJS
-await import( "@fortawesome/fontawesome-free/css/all.min.css" );
-await import( "./viewport/ext.override.masks.js" );
+import PrimeVue from "primevue/config";
 
 export default class VueExtViewport extends VueViewport {
 
@@ -21,40 +17,26 @@ export default class VueExtViewport extends VueViewport {
         await super.init();
 
         window[ Symbol.for( "app" ) ] = this.app;
-
-        return new Promise( resolve => {
-            Ext.application( {
-                "name": "-",
-                "quickTips": true,
-                "launch": resolve,
-            } );
-        } );
-    }
-
-    mount ( selector ) {
-
-        // mount to the Ext.Viewport
-        super.mount( Ext.Viewport.el.dom );
     }
 
     // protected
+    // XXX use router
     _createVauApp () {
         const vue = super._createVauApp();
 
-        // components requires .toString() method,
-        // because ext is trying to convert component to string when component is passed as scope to .on() call
-        vue.config.globalProperties.toString = function toString () {
-            return null;
-        };
+        // vue use( router )
+        vue.use( PrimeVue );
 
         return vue;
     }
 
     // private
+    // XXX
     #applyTheme () {
-        Ext.theme.Material.setColors( {
-            ...this.app.theme.theme,
-            "darkMode": this.app.theme.darkMode,
-        } );
+
+        // Ext.theme.Material.setColors( {
+        //     ...this.app.theme.theme,
+        //     "darkMode": this.app.theme.darkMode,
+        // } );
     }
 }
